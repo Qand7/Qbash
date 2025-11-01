122da9f051b3f040461ccb9a52186f3d74591b91
Qandy <145124536+Qand7@users.noreply.github.com>
2025-11-01 19:35:11 +0100

    Update install script to copy new scripts

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 GOALDIR="/usr/bin"
 
-sudo cp qpurge-qtest/qpurge "$GOALDIR"/qpurge
-sudo cp qpurge-qtest/qtest "$GOALDIR"/qtest
+sudo cp Qbash/Scripts/qpurge "$GOALDIR"/qpurge
+sudo cp Qabsh/Scripts/qtest "$GOALDIR"/qtest
+sudo cp Qbash/Scripts/qfind "$GOALDIR"/qfind
 
 sudo chmod +x "$GOALDIR"/qpurge
 sudo chmod +x "$GOALDIR"/qtest
+sudo chmod +x "$GOALDIR"/qfind
 
 echo "-------------------------------------------" 
-echo "Finished, qpurge and qtest are ready."
+echo "Finished, Scripts are ready."
 echo
-echo "Type \"qpurge -h\" or \"qtest -h\"for help."
+echo "Type \"<Scriptname> -h\" for help."

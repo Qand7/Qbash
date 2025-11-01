ed303294bf842b35f4165f745e517b5371c4cc8a
Qandy <145124536+Qand7@users.noreply.github.com>
2025-11-01 19:38:52 +0100

    Implement script installation in install.sh
    Added script installation and permission setup.

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Defining directory for the Scripts to land in
 GOALDIR="/usr/bin"
 
+# Creating Scripts
 sudo cp Qbash/Scripts/qpurge "$GOALDIR"/qpurge
 sudo cp Qabsh/Scripts/qtest "$GOALDIR"/qtest
 sudo cp Qbash/Scripts/qfind "$GOALDIR"/qfind
 
+# Giving Scripts execution permissions
 sudo chmod +x "$GOALDIR"/qpurge
 sudo chmod +x "$GOALDIR"/qtest
 sudo chmod +x "$GOALDIR"/qfind
 
+# Fiunishing message
 echo "-------------------------------------------" 
 echo "Finished, Scripts are ready."
 echo

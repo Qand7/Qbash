5216f2124b5f41ec9139f1defe10f4659cbd6b45
Qandy <145124536+Qand7@users.noreply.github.com>
2025-11-01 19:37:41 +0100

    Update delete.sh to remove scripts and permissions
    Removed script permissions and deleted related files.

--- delete.sh
+++ delete.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+#Removing Script permissions
 sudo chmod -x /usr/bin/qpurge
 sudo chmod -x /usr/bin/qtest
+sudo chmod -x /usr/bin/qfind
+
+# Removing the scripts and source directory
 sudo rm -rf /usr/bin/qtest
 sudo rm -rf /usr/bin/qpurge
-sudo rm -rf ~/qpurge-qtest
+sudo rm -rf /usr/bin/qfind
 
+sudo rm -rf ~/Qbash
+
+# Finishing message
 echo "-------------------"
 echo "Deleted all Files."
